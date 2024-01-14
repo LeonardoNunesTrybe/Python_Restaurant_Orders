61caae1e08aefe4e8163b75674297a5139a5563e
Leonardo Nunes <leonardonunes.uberbraga@gmail.com>
2024-01-14 23:45:45 +0000

    feat: requisito 1

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,12 +1,20 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction  # noqa: F401, E261, E501
 
 
 # Req 1
-def test_ingredient():
-    pass
 
-# a classe pode ser instanciada corretamente de acordo com a assinatura esperada;
-# o atributo conjunto restrictions é populado como esperado;
-# o método mágico __repr__ funcione como esperado;
-# o método mágico __eq__ funcione como esperado;
-# o método mágico __hash__ funcione como esperado.
+def test_ingredient():
+    ingredient = Ingredient('manteiga')
+    assert ingredient.name == 'manteiga'
+    assert ingredient.restrictions == {
+        Restriction.LACTOSE,
+        Restriction.ANIMAL_DERIVED
+    }
+
+    assert repr(ingredient) == "Ingredient('manteiga')"
+
+    assert hash(ingredient) == hash('manteiga')
+    assert hash(ingredient) != hash('salmão')
+
+    assert ingredient.__eq__(Ingredient('manteiga')) is True
+    assert ingredient.__eq__(Ingredient('salmão')) is False
