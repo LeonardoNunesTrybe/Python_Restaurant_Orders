4ad5431354dcddfe98f2f4f864c63fde7fa6cd49
Leonardo Nunes <leonardonunes.uberbraga@gmail.com>
2024-01-14 23:14:29 +0000

    iniciando projeto

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -4,3 +4,9 @@ from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
 # Req 1
 def test_ingredient():
     pass
+
+# a classe pode ser instanciada corretamente de acordo com a assinatura esperada;
+# o atributo conjunto restrictions é populado como esperado;
+# o método mágico __repr__ funcione como esperado;
+# o método mágico __eq__ funcione como esperado;
+# o método mágico __hash__ funcione como esperado.
